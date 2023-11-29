222219cebb40ea301f6d63979eadfc03e4be7bbf
Motaz HM <motazhajm@gmail.com>
2023-11-29 18:55:16 +0200

    Add sessions logic

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
 const path = require("path");
 
 const express = require("express");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
 
 const db = require("./data/database");
 const shopRoutes = require("./shop/routes");
 
+const MongoDBStore = mongodbStore(session);
+
 const app = express();
+
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://127.0.0.1.:27017",
+  databaseName: "online-shop",
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "test",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
+app.use(async (req, res, next) => {
+  const user = req.session.user;
+  const isAuthenticated = req.session.isAuthenticated;
+
+  if (!isAuthenticated) {
+    return next();
+  }
+
+  const userData = await db.getDb().collection("users").fineOnde({
+    _id: user.id,
+  });
+  const isAdmin = userData.isAdmin;
+
+  res.locals.isAuthenticated = isAuthenticated;
+  res.locals.isAdmin = isAdmin;
+  next();
+});
 
 app.use(shopRoutes);
 
