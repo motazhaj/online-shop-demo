a58b745ec0aca44990ce087d3d0f43f24bb6b1ee
Motaz HM <motazhajm@gmail.com>
2023-11-29 12:59:42 +0200

    Implement sign up page logic

--- shop/routes.js
+++ shop/routes.js
@@ -6,4 +6,14 @@ router.get("/", (req, res) => {
   res.render("shop");
 });
 
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.post("/signup", async (req, res) => {
+  const userData = req.body
+  console.log(userData)
+  res.redirect("/")
+});
+
 module.exports = router;
