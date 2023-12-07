8f93f3f739c2f7608978e48da652d305762e902c
Motaz HM <motazhajm@gmail.com>
2023-12-07 16:21:30 +0200

    Add name greeting feature

--- utilities/authentication.js
+++ utilities/authentication.js
@@ -1,6 +1,7 @@
 function createUserSession(req, user, action) {
   req.session.uid = user._id.toString();
   req.session.isAdmin = user.isAdmin;
+  req.session.name = user.name;
   req.session.save(action);
 }
 
