7d4d7a9ec640733e564b0ac76c0601b1c3fe79ea
Motaz HM <motazhajm@gmail.com>
2023-12-26 00:09:27 +0200

    Add products list display

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,5 +1,13 @@
-function getShop(req, res) {
-  res.render("customer/shop");
+const Product = require("../models/product.model");
+
+async function getShop(req, res, next) {
+  try {
+    const products = await Product.findAll();
+    res.render("customer/shop", { products: products });
+  } catch (error) {
+    next();
+    return;
+  }
 }
 
 module.exports = {
